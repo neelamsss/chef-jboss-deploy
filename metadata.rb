9e4bc78fb69bec9dde861171fe05c5c74b54b4c2
jantwisted <janith@member.fsf.org>
2016-08-23 23:00:53 +0530

    License added

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,11 @@ license          'All rights reserved'
 description      'Deploy applications to Jboss-eap'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+license          'GPLv3'
+source_url	 'https://github.com/jantwisted/chef-jboss-deploy'
+issues_url	 'https://github.com/jantwisted/chef-jboss-deploy'
+
+supports 'redhat'
+supports 'debian'
+supports 'centos'
+supports 'ubuntu'
