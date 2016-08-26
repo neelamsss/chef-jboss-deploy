46725ef4d44d468a834d1ecf1ac6b907c2b8b728
jantwisted <janith@member.fsf.org>
2016-08-26 10:08:29 -0400

    Properties added

--- resources/datasource.rb
+++ resources/datasource.rb
@@ -1,5 +1,17 @@
 resource_name :datasource
 
+property :_owner, String, default: 'root'
+property :_host, String, default: '127.0.0.1'
+property :_offset, Integer, default: 0
+property :_jboss_mode, String, default: 'standalone'
+property :_jboss_home, String, default: '/opt/jboss/'
+property :mgmtport, Integer, default: 9999
+property :_jdbc_driver_name, String
+property :_user, String
+property :_password, String
+property :_dt_name, String
+property :_connection_url, String
+property :_jndi_name, String
 
 action :create do
   
--- resources/module.rb
+++ resources/module.rb
@@ -1,5 +1,17 @@
 resource_name :module
 
+
+property :_owner, String, default: 'root'
+property :_module_name, String
+property :_host, String, default: '127.0.0.1'
+property :_offset, Integer, default: 0
+property :_jboss_mode, String, default: 'standalone'
+property :_jboss_home, String, default: '/opt/jboss/'
+property :mgmtport, Integer, default: 9999
+property :_jdbc_loc, String
+property :_dep_list, String
+property :_jdbc_driver_name, String
+
 action :create do 
 
     execute 'module-add' do
