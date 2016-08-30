1edc60b4df93f82b65dd84ac3b274abc48a6b454
Janith Perera <janith@member.fsf.org>
2016-08-30 12:26:47 -0400

    Update datasource.rb

--- resources/datasource.rb
+++ resources/datasource.rb
@@ -1,30 +1,29 @@
 resource_name :datasource
-
-property :_owner, String, default: 'root'
-property :_host, String, default: '127.0.0.1'
-property :_offset, Integer, default: 0
-property :_jboss_mode, String, default: 'standalone'
-property :_jboss_home, String, default: '/opt/jboss/'
+property :owner, String, default: 'root'
+property :host, String, default: '127.0.0.1'
+property :offset, Integer, default: 0
+property :jboss_mode, String, default: 'standalone'
+property :jboss_home, String, default: '/opt/jboss/'
 property :mgmtport, Integer, default: 9999
-property :_jdbc_driver_name, String
-property :_user, String
-property :_password, String
-property :_dt_name, String
-property :_connection_url, String
-property :_jndi_name, String
+property :jdbc_driver_name, String
+property :user, String
+property :password, String
+property :dt_name, String
+property :connection_url, String
+property :jndi_name, String
 
 action :create do
   
   execute 'data-source' do
-    user _owner
-    command _jboss_home+
+    user owner
+    command jboss_home+
       '/bin/jboss-cli.sh --connect controller='+
-      _host+':'+
-      "#{mgmtport+_offset}"+
+      host+':'+
+      "#{mgmtport+offset}"+
       ' --commands="data-source add --jndi-name=java:/'+
-      _jndi_name+' --name='+_dt_name+' --connection-url='+
-      _connection_url+' --driver-name='+_jdbc_driver_name+
-      ' --user-name='+_user+' --password='+_password+' --enabled=true"'
+      jndi_name+' --name='+dt_name+' --connection-url='+
+      connection_url+' --driver-name='+jdbc_driver_name+
+      ' --user-name='+user+' --password='+password+' --enabled=true"'
   end
   
 end
