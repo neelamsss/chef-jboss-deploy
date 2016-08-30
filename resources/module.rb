b60f93f8b0663301790ea2625a2b58616b9e5c69
Janith Perera <janith@member.fsf.org>
2016-08-30 12:24:17 -0400

    Variable change
    minor changes

--- resources/module.rb
+++ resources/module.rb
@@ -1,38 +1,38 @@
 resource_name :jmodule
 
 
-property :_owner, String, default: 'root'
-property :_module_name, String
-property :_host, String, default: '127.0.0.1'
-property :_offset, Integer, default: 0
-property :_jboss_mode, String, default: 'standalone'
-property :_jboss_home, String, default: '/opt/jboss/'
+property :owner, String, default: 'root'
+property :module_name, String
+property :host, String, default: '127.0.0.1'
+property :offset, Integer, default: 0
+property :jboss_mode, String, default: 'standalone'
+property :jboss_home, String, default: '/opt/jboss/'
 property :mgmtport, Integer, default: 9999
-property :_jdbc_loc, String
-property :_dep_list, String
-property :_jdbc_driver_name, String
-property :_class_name, String
+property :jdbc_loc, String
+property :dep_list, String
+property :jdbc_driver_name, String
+property :class_name, String
 
 action :create do 
 
   execute 'module-add' do
-    user _owner
-    command _jboss_home+
+    user owner
+    command jboss_home+
       '/bin/jboss-cli.sh --connect controller='+
-      _host+':'+
-      "#{mgmtport+_offset}"+
-      ' --commands="module add --name='+_module_name+' --resources='+_jdbc_loc+' --dependencies='+_dep_list+'"'
+      host+':'+
+      "#{mgmtport+offset}"+
+      ' --commands="module add --name='+module_name+' --resources='+jdbc_loc+' --dependencies='+dep_list+'"'
   end
   execute 'install-jdbc' do
-    user _owner
-    command _jboss_home+
+    user owner
+    command jboss_home+
       '/bin/jboss-cli.sh --connect controller='+
-      _host+':'+
+      host+':'+
       "#{mgmtport+_offset}"+
       ' --commands="/subsystem=datasources/jdbc-driver='+
-      _jdbc_driver_name+':add(driver-name='+
-      _jdbc_driver_name+',driver-module-name='+
-      _module_name+',driver-class-name='+_class_name+')"'
+      jdbc_driver_name+':add(driver-name='+
+      jdbc_driver_name+',driver-module-name='+
+      module_name+',driver-class-name='+class_name+')"'
   end
 
 end
